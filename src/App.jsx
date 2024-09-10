ccba99613d363f73c6c63a2e7b3b4f370a186ac7
Damodharkarthu123 <damodharkarthu123@gmail.com>
2024-09-10 16:54:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+
+import React from 'react'
+ 
+let a = "javascript xml"
+let b = "the coders "
+
+function App() {
+  return (
+    <div>
+     <h1>{a}</h1>
+     <ul>
+      <li>dhamu</li>
+      <li>avi</li>
+      <li>meggu</li>
+      <li>{b}</li>
+     </ul>
+     <form>
+      <label>student name</label>
+      <input type="text"></input>
+     </form>
+    </div>
+  )
+}
+
+export default App
